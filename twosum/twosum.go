066fd942f314697370a3b23f168ae428c9d51daf
Foudil Bretel <519093+foudfou@users.noreply.github.com>
2025-07-11 23:32:52 +0000

    Add TwoSumOofN to twosum package

--- twosum/twosum.go
+++ twosum/twosum.go
@@ -1,5 +1,22 @@
 package twosum
 
+// The classic Two Sum problem statement says "You may assume that each input
+// would have exactly one solution" - so there's guaranteed to be exactly one
+// valid pair.
+func TwoSumOofN(a []int, target int) []int {
+	seen := map[int]int{}
+
+	for i, v := range a {
+		k, ok := seen[target-v] // test presence
+		if ok {
+			return []int{k, i}
+		}
+		seen[v] = i
+	}
+
+	return []int{}
+}
+
 func TwoSum(a []int, target int) [][]int {
 	sums := [][]int{}
 	for i := 0; i < len(a); i++ {
--- twosum/twosum_test.go
+++ twosum/twosum_test.go
@@ -5,6 +5,30 @@ import (
 	"testing"
 )
 
+func TestTwoSumOofN(t *testing.T) {
+	cases := []struct {
+		array  []int
+		target int
+		want   []int
+	}{
+		{[]int{2, 3, 5, 7, 11}, 12, []int{2, 3}},
+		{[]int{2, 3, 5, 7, 11, 3}, 10, []int{1, 3}},
+		{[]int{2, 3, 5, 7, 11}, 4, []int{}},
+		{[]int{}, 0, []int{}},
+	}
+
+	for _, tt := range cases {
+		t.Run("twosum O(n)", func(t *testing.T) {
+			got := TwoSumOofN(tt.array, tt.target)
+			// fmt.Printf("got=%v, want=%v", got, tt.want)
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v want %v given, %v and target %d", got, tt.want, tt.array, tt.target)
+			}
+		})
+	}
+}
+
 func TestTwoSum(t *testing.T) {
 	cases := []struct {
 		array  []int
